37eeb753474622e391429c4bf844b397d7a6412a
Gaurav <gg.spark@gmail.com>
2015-05-29 02:19:43 +0530

    Added micro nutrients

--- Holmusk/app/src/main/java/play/gaurav/holmusk/activites/BaseActivity.java
+++ Holmusk/app/src/main/java/play/gaurav/holmusk/activites/BaseActivity.java
@@ -34,6 +34,7 @@ public class BaseActivity extends AppCompatActivity {
     protected List<FoodItem> foodItemList;
     protected BarChart chart1;
     protected PieChart chart2;
+    protected BarChart chart3;
 
 
     @Override
@@ -84,6 +85,10 @@ public class BaseActivity extends AppCompatActivity {
 
         chart2 = (PieChart) findViewById(R.id.chart2);
         setupPieChart(chart2);
+
+        chart3 = (BarChart) findViewById(R.id.chart3);
+        setupBarChart(chart3);
+
     }
 
     private static void setupPieChart(PieChart chart) {
@@ -123,66 +128,104 @@ public class BaseActivity extends AppCompatActivity {
 
     protected void setBarData(FoodItem item) {
 
-        float protein = getFloat(item.getMeta().getProtein());
-        float carbs =getFloat(item.getMeta().getCarbohydrate());
-        float fat= getFloat(item.getMeta().getFat());
-        float totalCal = protein*4 + carbs*4 + fat*9;
+        {
+            float protein = getFloat(item.getMeta().getProtein());
+            float carbs = getFloat(item.getMeta().getCarbohydrate());
+            float fat = getFloat(item.getMeta().getFat());
+            float totalCal = protein * 4 + carbs * 4 + fat * 9;
 
-        ArrayList<String> xVals = new ArrayList<String>();
-        xVals.add(getString(R.string.protein));
-        xVals.add(getString(R.string.carb));
-        xVals.add(getString(R.string.fat));
+            ArrayList<String> xVals = new ArrayList<String>();
+            xVals.add(getString(R.string.protein));
+            xVals.add(getString(R.string.carb));
+            xVals.add(getString(R.string.fat));
 
-        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
-        yVals1.add(new BarEntry(protein, 0));
-        yVals1.add(new BarEntry(carbs, 1));
-        yVals1.add(new BarEntry(fat, 2));
+            ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+            yVals1.add(new BarEntry(protein, 0));
+            yVals1.add(new BarEntry(carbs, 1));
+            yVals1.add(new BarEntry(fat, 2));
 
-        BarDataSet set1 = new BarDataSet(yVals1, "Data Set");
-        set1.setColors(ColorTemplate.VORDIPLOM_COLORS);
-        set1.setDrawValues(true);
+            BarDataSet set1 = new BarDataSet(yVals1, "Data Set");
+            set1.setColors(ColorTemplate.VORDIPLOM_COLORS);
+            set1.setDrawValues(true);
 
-        ArrayList<BarDataSet> dataSets = new ArrayList<BarDataSet>();
-        dataSets.add(set1);
+            ArrayList<BarDataSet> dataSets = new ArrayList<BarDataSet>();
+            dataSets.add(set1);
 
-        BarData data = new BarData(xVals, dataSets);
-        data.setValueTextSize(10f);
+            BarData data = new BarData(xVals, dataSets);
+            data.setValueTextSize(10f);
 
-        chart1.setData(data);
-        chart1.invalidate();
-        chart1.animateY(DURATION_MILLIS);
-
-
-
+            chart1.setData(data);
+            chart1.invalidate();
+            chart1.animateY(DURATION_MILLIS);
 
 
+            ArrayList<Entry> yVals2 = new ArrayList<Entry>();
+            yVals2.add(new Entry(protein * 4 / totalCal, 0));
+            yVals2.add(new Entry(carbs * 4 / totalCal, 1));
+            yVals2.add(new Entry(fat * 9 / totalCal, 2));
+
+            PieDataSet dataSet = new PieDataSet(yVals2, getString(R.string.cal_dist));
+            dataSet.setSliceSpace(3f);
+            dataSet.setSelectionShift(5f);
+            dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
+
+            PieData pieData = new PieData(xVals, dataSet);
+            pieData.setValueFormatter(new PercentFormatter());
+            pieData.setValueTextSize(10f);
+            pieData.setValueTextColor(Color.BLACK);
+
+            chart2.setData(pieData);
+            chart2.setCenterText(getFloat(item.getMeta().getEnergy()) + getString(R.string.calories));
+            chart2.highlightValues(null); //Remove all highlights
+            chart2.invalidate();
+            chart2.animateY(DURATION_MILLIS);
+        }
 
 
-        ArrayList<Entry> yVals2 = new ArrayList<Entry>();
-        yVals2.add(new Entry(protein*4/totalCal, 0));
-        yVals2.add(new Entry(carbs*4/totalCal, 1));
-        yVals2.add(new Entry(fat * 9 / totalCal, 2));
+        {
+            float sodium = getFloat(item.getMeta().getSodium());
+            float potassium = getFloat(item.getMeta().getPotassium());
+            float cholesterol = getFloat(item.getMeta().getCholesterol());
 
-        PieDataSet dataSet = new PieDataSet(yVals2, getString(R.string.cal_dist));
-        dataSet.setSliceSpace(3f);
-        dataSet.setSelectionShift(5f);
-        dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
+            ArrayList<String> xVals = new ArrayList<String>();
+            xVals.add("Sodium");
+            xVals.add("Potassium");
+            xVals.add("Cholesterol");
 
-        PieData pieData = new PieData(xVals, dataSet);
-        pieData.setValueFormatter(new PercentFormatter());
-        pieData.setValueTextSize(10f);
-        pieData.setValueTextColor(Color.BLACK);
+            ArrayList<BarEntry> yVals = new ArrayList<BarEntry>();
+            yVals.add(new BarEntry(sodium, 0));
+            yVals.add(new BarEntry(potassium, 1));
+            yVals.add(new BarEntry(cholesterol, 2));
 
-        chart2.setData(pieData);
-        chart2.setCenterText(getFloat(item.getMeta().getEnergy()) + getString(R.string.calories));
-        chart2.highlightValues(null); //Remove all highlights
-        chart2.invalidate();
-        chart2.animateY(DURATION_MILLIS);
+            BarDataSet set = new BarDataSet(yVals, "Data Set");
+            set.setColors(ColorTemplate.JOYFUL_COLORS);
+            set.setDrawValues(true);
+
+            ArrayList<BarDataSet> dataSets = new ArrayList<BarDataSet>();
+            dataSets.add(set);
+
+            BarData data = new BarData(xVals, dataSets);
+            data.setValueTextSize(10f);
+
+            chart3.setData(data);
+            chart3.invalidate();
+            chart3.animateY(DURATION_MILLIS);
+        }
+
+
+
 
     }
 
     protected float getFloat(String item){
-        StringTokenizer st = new StringTokenizer(item);
-        return Float.parseFloat(st.nextToken());
+        if(item==null)
+            return 0;
+        try {
+            StringTokenizer st = new StringTokenizer(item);
+            return Float.parseFloat(st.nextToken());
+        }catch (Exception e)
+        {
+            return 0;
+        }
     }
 }
